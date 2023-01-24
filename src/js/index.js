acfc636ff3ad5908d665c068536a16f20707c6c4
JeanG1914 <jeanpaulgzea@gmail.com>
2023-01-24 02:31:24 +0000

    almost done

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 //import react into the bundle
-import React from "react";
+import React ,{ useState , useEffect } from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import Trafficlight from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Trafficlight />, document.querySelector("#app"));
